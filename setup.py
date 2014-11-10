3b680613e4d9516b2b1aa110c28a4aacabfe060f
Leonardo.Z <glasslion@gmail.com>
2014-11-10 11:47:56 +0800

    Read packages from README

--- setup.py
+++ setup.py
@@ -5,10 +5,9 @@ from os import path
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the relevant file
-# with open(path.join(here, 'DESCRIPTION.rst'), encoding='utf-8') as f:
-#     long_description = f.read()
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
 
-long_description = ""
 
 setup(
     name='redlock',
