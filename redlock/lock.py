e5065ea214f600743368197035e411e1154ff0b6
Leonardo.Z <glasslion@gmail.com>
2014-11-10 18:11:27 +0800

    Fix missing doc strings

--- redlock/lock.py
+++ redlock/lock.py
@@ -1,4 +1,6 @@
-"""Distributed locks with Redis
+"""
+Distributed locks with Redis
+Redis doc: http://redis.io/topics/distlock
 """
 from __future__ import division
 from datetime import datetime
@@ -11,8 +13,11 @@ import redis
 
 DEFAULT_RETRY_TIMES = 3
 DEFAULT_RETRY_DELAY = 200
-DEFAULT_TTL = 1000
+DEFAULT_TTL = 100000
 CLOCK_DRIFT_FACTOR = 0.01
+
+# Reference:  http://redis.io/topics/distlock
+# Section Correct implementation with a single instance
 RELEASE_LUA_SCRIPT = """
     if redis.call("get",KEYS[1]) == ARGV[1] then
         return redis.call("del",KEYS[1])
@@ -22,13 +27,19 @@ RELEASE_LUA_SCRIPT = """
 """
 
 
-class RedLockReleaseError(Exception):
+class RedLockError(Exception):
     pass
 
 
 class RedLockFactory(object):
+    """
+    A Factory class that help reuse multiple Redis conections.
+    """
 
     def __init__(self, connection_details):
+        """
+
+        """
         self.redis_nodes = []
 
         for conn in connection_details:
@@ -38,6 +49,11 @@ class RedLockFactory(object):
             self.quorum = len(self.redis_nodes) // 2 + 1
 
     def create_lock(self, resource, **kwargs):
+        """
+        Create a new RedLock object and reuse stored Redis clients.
+        All the kwargs it received would be passed to the RedLock's __init__
+        function.
+        """
         lock = RedLock(resource=resource, created_by_factory=True, **kwargs)
         lock.redis_nodes = self.redis_nodes
         lock.quorum = self.quorum
@@ -48,6 +64,9 @@ class RedLockFactory(object):
 class RedLock(object):
 
     """
+    A distributed lock implementation based on Redis.
+    It shares a similar API with the `threading.Lock` class in the
+    Python Standard Library.
     """
 
     def __init__(self, resource, connection_details=None,
@@ -88,18 +107,28 @@ class RedLock(object):
         self.release()
 
     def acquire_node(self, node):
+        """
+        accquire a single redis ndoe
+        """
         return node.set(self.resource, self.lock_key, nx=True, px=self.ttl)
 
     def release_node(self, node):
+        """
+        release a single redis ndoe
+        """
+        # use the lua script to release the lock in a safe way
         node._release_script(keys=[self.resource], args=[self.lock_key])
 
     def acquire(self):
+
+        # lock_key should be random and unique
         self.lock_key = uuid.uuid4().hex
 
         for retry in range(self.retry_times):
             acquired_node_count = 0
             start_time = datetime.utcnow()
 
+            # acquire the lock in all the redis instances sequentially
             for node in self.redis_nodes:
                 if self.acquire_node(node):
                     acquired_node_count += 1
