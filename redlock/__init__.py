7392dcf4da9bf7903d0c346130fe456bda514ae2
Leonardo.Z <glasslion@gmail.com>
2014-11-10 17:32:55 +0800

    Bump version: 0.1.4 → 0.2.0

--- redlock/__init__.py
+++ redlock/__init__.py
@@ -1,2 +1,2 @@
 from .lock import RedLock, RedLockFactory
-__VERSION__ = '0.1.4'
+__VERSION__ = '0.2.0'
--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # http://packaging.python.org/en/latest/tutorial.html#version
-    version='0.1.4',
+    version='0.2.0',
 
     description='Distributed locks with Redis',
     long_description=long_description,
